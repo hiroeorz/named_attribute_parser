af641d92a4dec419f6bc2f87e9cd20a25dd04470
HIROE Shin <hiroe.orz@gmail.com>
2011-12-25 16:18:40 +0900

    spec for block.

--- pkg/named_attribute_parser-0.0.1/spec/named_attribute_parser_spec.rb
+++ pkg/named_attribute_parser-0.0.1/spec/named_attribute_parser_spec.rb
@@ -7,25 +7,20 @@ describe NamedAttributeParser do
     @obj.extend NamedAttributeParser
 
     @obj.instance_eval do
-      def default_name; "TARO" end
-      def default_age; 0 end
-      def default_address; "TOKYO" end
+      def def_name; "TARO" end
+      def def_age; 0 end
+      def def_address; "TOKYO" end
 
       def get_parsed(*args)
         name, age, address = 
-          named_attributes(args, :name , 
-                           age: default_age, address: default_address)
+          named_attributes(args, :name, age: def_age, address: def_address)
         
         [name, age, address]
       end
 
       def get_parsed_all_default(*args)
         name, age, address = 
-          named_attributes(args, 
-                           name: default_name , 
-                           age: default_age, 
-                           address: default_address)
-        
+          named_attributes(args, name: def_name, age: def_age, address: def_address)
         [name, age, address]
       end
     end
@@ -40,6 +35,16 @@ describe NamedAttributeParser do
     address.should == "SHIMANE"
   end
 
+  it "parse hash attibutes" do
+    name, age, address = 
+      @obj.get_parsed(:name => "shin", :age => 36, :address => "SHIMANE")
+
+    name.should == "shin"
+    age.should == 36
+    address.should == "SHIMANE"
+  end
+
+
   it "parse named attibutes" do
     name, age, address = 
       @obj.get_parsed(name: "shin", age: 36, address: "SHIMANE")
@@ -53,21 +58,103 @@ describe NamedAttributeParser do
     name, age, address = @obj.get_parsed(name: "shin", age: 36)
     name.should == "shin"
     age.should == 36
-    address.should == @obj.default_address
+    address.should == @obj.def_address
   end
 
   it "parse named attibutes using 2 default value" do
     name, age, address = @obj.get_parsed(name: "shin")
     name.should == "shin"
-    age.should == @obj.default_age
-    address.should == @obj.default_address
+    age.should == @obj.def_age
+    address.should == @obj.def_address
   end
 
   it "parse named attibutes using 2 default value" do
     name, age, address = @obj.get_parsed_all_default
-    name.should == @obj.default_name
-    age.should == @obj.default_age
-    address.should == @obj.default_address
+    name.should == @obj.def_name
+    age.should == @obj.def_age
+    address.should == @obj.def_address
+  end
+
+  it "should raise if named attribute not given than is not given default" do
+    lambda{ @obj.get_parsed }.should raise_error ArgumentError
+  end
+  
+end
+
+describe "NamedAttributeParser with block" do
+  
+  before :all do
+    @obj = Object.new
+    @obj.extend NamedAttributeParser
+    
+    @obj.instance_eval do
+      def def_name; "TARO" end
+      def def_age; 0 end
+      def def_address; "TOKYO" end
+
+      def get_parsed(*args)
+        named_attributes(args, :name, age: def_age, address: def_address) do
+          | name, age, address |
+          [name, age, address]
+        end
+      end
+
+      def get_parsed_all_default(*args)
+        named_attributes(args, name: def_name, age: def_age, address: def_address) do
+          | name, age, address |
+          [name, age, address]
+        end
+      end
+    end
+  end
+
+  it "parse hash attibutes" do
+    name, age, address = 
+      @obj.get_parsed(:name => "shin", :age => 36, :address => "SHIMANE")
+
+    name.should == "shin"
+    age.should == 36
+    address.should == "SHIMANE"
+  end
+
+  it "parse hash attibutes" do
+    name, age, address = 
+      @obj.get_parsed(:name => "shin", :age => 36, :address => "SHIMANE")
+
+    name.should == "shin"
+    age.should == 36
+    address.should == "SHIMANE"
+  end
+
+
+  it "parse named attibutes" do
+    name, age, address = 
+      @obj.get_parsed(name: "shin", age: 36, address: "SHIMANE")
+
+    name.should == "shin"
+    age.should == 36
+    address.should == "SHIMANE"
+  end
+
+  it "parse named attibutes using 1 default value" do
+    name, age, address = @obj.get_parsed(name: "shin", age: 36)
+    name.should == "shin"
+    age.should == 36
+    address.should == @obj.def_address
+  end
+
+  it "parse named attibutes using 2 default value" do
+    name, age, address = @obj.get_parsed(name: "shin")
+    name.should == "shin"
+    age.should == @obj.def_age
+    address.should == @obj.def_address
+  end
+
+  it "parse named attibutes using 2 default value" do
+    name, age, address = @obj.get_parsed_all_default
+    name.should == @obj.def_name
+    age.should == @obj.def_age
+    address.should == @obj.def_address
   end
 
   it "should raise if named attribute not given than is not given default" do
